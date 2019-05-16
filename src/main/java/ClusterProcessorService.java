8955df70a23864a1bf5cbdbc9971c590d0bf8827
Jona Otholt <18723283+J-L-O@users.noreply.github.com>
2019-05-16 15:17:41 +0200

    Remove unused imports.

--- src/main/java/ClusterProcessorService.java
+++ src/main/java/ClusterProcessorService.java
@@ -1,22 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements. See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.common.serialization.IntegerDeserializer;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.streams.KafkaStreams;
@@ -31,9 +12,7 @@ import org.apache.kafka.streams.state.KeyValueIterator;
 import org.apache.kafka.streams.state.KeyValueStore;
 import org.apache.kafka.streams.state.Stores;
 
-import java.time.Duration;
 import java.util.ArrayList;
-import java.util.Locale;
 import java.util.Properties;
 import java.util.concurrent.CountDownLatch;
 
@@ -62,7 +41,7 @@ public final class ClusterProcessorService {
 
 
                     if(!clusters.hasNext()){
-                        Cluster dummy = new Cluster();
+                        Cluster dummy = new Cluster(2);
                         dummy.size = 1;
                         dummy.linearSums = new double[]{3, 4};
                         dummy.minimums = new double[]{3, 4};
