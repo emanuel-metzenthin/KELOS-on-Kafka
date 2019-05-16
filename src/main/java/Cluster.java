401242e4fa9c3762b77427abde0890143f68e7c8
Jona Otholt <18723283+J-L-O@users.noreply.github.com>
2019-05-16 11:02:59 +0200

    Add Cluster methods.

--- src/main/java/Cluster.java
+++ src/main/java/Cluster.java
@@ -1,10 +1,29 @@
 public class Cluster {
     int size;
+    double[] centroid;
     double[] linearSums;
     double[] minimums;
     double[] maximums;
 
-    public double distance(double[] x) {
-        return 0.0;
+    public double distance(double[] record) {
+
+        double sum_of_squares = 0;
+
+        for(int i = 0; i < record.length; i++){
+            double difference = record[i] - this.centroid[i];
+            sum_of_squares += difference * difference;
+        }
+
+        return Math.sqrt(sum_of_squares);
+    }
+
+    public void addRecord(double[] record){
+        this.size += 1;
+
+        for(int i = 0; i < record.length; i++){
+            this.linearSums[i] += record[i];
+            this.centroid[i] = this.linearSums[i] / this.size;
+            // Do not update minimums and maximums because they are not needed at this stage
+        }
     }
 }
