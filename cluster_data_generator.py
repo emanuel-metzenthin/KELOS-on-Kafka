4ab907037939d87716a1e1f680278899a48b7bc5
Emanuel Metzenthin <emanuel.metzenthin@student.hpi.de>
2019-06-03 12:05:21 +0200

    Add python data generator file

---
+++ cluster_data_generator.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+cluster_1 = np.random.multivariate_normal([0, 0, 0], [[1, 0, 0], [0, 3, 0], [0, 0, 1]], 400)
+cluster_2 = np.random.multivariate_normal([4, 4, 4], [[1.5, 0, 0], [0, 1, 0], [0, 0, 1]], 400)
+cluster_3 = np.random.multivariate_normal([10, -5, 3], [[1, 0, 0], [0, 3, 0], [0, 0, 10]], 400)
+cluster_4 = np.random.multivariate_normal([-2, -15, -5], [[1, 0, 0], [0, 3, 0], [0, 0, 10]], 400)
+
+data = np.concatenate((cluster_1, cluster_2, cluster_3, cluster_4))
+print(data)
+
+df = pd.DataFrame(data)
+
+plt.scatter(df[0], df[2])
+
+df.to_csv('cluster_test_data.csv')
