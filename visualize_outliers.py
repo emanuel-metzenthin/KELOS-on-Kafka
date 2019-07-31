35291cbb45bd5933023606ebe94be9faaf20cb5e
Jona Otholt <18723283+J-L-O@users.noreply.github.com>
2019-07-31 18:48:50 +0200

    Add script to plot the candidates and outliers

---
+++ visualize_outliers.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+import scipy.stats
+import matplotlib.pyplot as plt
+
+mean = [0, 0]
+cov = [[1, 0], [0, 1]]
+
+
+data = pd.read_csv('test_data_labeled.csv', header=None)
+
+candidates = pd.read_csv('candidates.csv', header=None)
+outliers = pd.read_csv('outliers.csv', header=None)
+zeros = pd.DataFrame(np.zeros(data.shape[0]))
+zeros.iloc[candidates[0]] = 2
+zeros.iloc[outliers[0]] = 1
+
+
+colors = {0:'white', 2:'yellow', 1:'red'}
+plt.scatter(data[0], data[1], c=data[2].apply(lambda x: colors[x]))
+plt.show()
+
+plt.scatter(data[0].iloc[candidates[0]], data[1].iloc[candidates[0]], c=zeros[0].iloc[candidates[0]].apply(lambda x: colors[x]))
+plt.show()
+
+
