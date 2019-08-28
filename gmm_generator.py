5833e8e4746185fbb059850ca6c4f928adaa9d36
Jona Otholt <18723283+J-L-O@users.noreply.github.com>
2019-08-28 12:15:59 +0200

    Generate ground truth outlier labels based on LOF.

--- gmm_generator.py
+++ gmm_generator.py
@@ -2,8 +2,9 @@ import pandas as pd
 import numpy as np
 import scipy.stats
 import matplotlib.pyplot as plt
+from sklearn.neighbors import LocalOutlierFactor
 
-total_samples = 10000
+total_samples = 30000
 weights = [0.6, 0.3, 0.1]
 means = [[0, 0], [5, 0], [3, 4]]
 covs = [[[1, -0.5], [-0.5, 1]], [[1, 0.2], [0.2, 1]], [[2, 0], [0, 2]]]
@@ -19,9 +20,16 @@ for i in range(len(weights)):
 	
 data = np.concatenate(results, axis=0)
 np.random.shuffle(data)
-df = pd.DataFrame(data)
+
+clf = LocalOutlierFactor(n_neighbors=200, contamination=0.01)
+outliers = pd.DataFrame(clf.fit_predict(data))
+
+df = pd.DataFrame(np.concatenate([data, outliers], axis=1))
+
 
 df[[0, 1]].to_csv('gmm_test_data_unlabeled.csv', index=False, header=False, float_format='%.5f')
+df.to_csv('gmm_test_data_labeled.csv', index=False, header=False, float_format='%.5f')
 
-plt.scatter(df[0], df[1], s=10)
+colors = {1:'blue', -1:'red'}
+plt.scatter(df[0], df[1], s=10, c=df[2].apply(lambda x: colors[x]))
 plt.show()
